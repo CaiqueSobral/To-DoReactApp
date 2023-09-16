77515f9ae61ec8b59e785c398eaacf58211ca293
Kiki <caiquelsobral@gmail.com>
2023-09-16 20:28:01 -0300

    Feat: Playing around

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Button, Text, Touchable, TouchableOpacity, View } from 'react-native';
+
+const hello = 'Salve World';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-600">
-      <Text className="text-gray-50">
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <View className="px-8 py-4 flex justify-center items-center bg-gray-400 rounded-md">
+        <Text className="text-gray-50">{hello}</Text>
+      </View>
+      <Text className="text-gray-50 my-5">Aqui tem mais texto!</Text>
+      <TouchableOpacity className="px-8 py-4 flex justify-center items-center bg-gray-400 rounded-md">
+        <Text className="text-gray-50">Tap Me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }

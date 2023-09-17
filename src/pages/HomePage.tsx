cb57081d8e28cebe8af100423f4fc19aaddbe33b
Kiki <caiquelsobral@gmail.com>
2023-09-17 14:00:39 -0300

    Refac: Refactor homepage to apply the changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 
 import GoalItem from '../components/GoalItem';
 import GoalInput from '../components/GoalInput';
-import GoalAddButton from '../components/GoalAddButton';
 
 export default function HomePage() {
   type goalType = {
@@ -11,23 +10,16 @@ export default function HomePage() {
     key: number;
   };
 
-  const [enteredGoalText, setEnteredGoalText] = useState<string>('');
   const [goals, setGoals] = useState<Array<goalType>>([]);
 
-  function goalInputTextHandler(enteredText: string) {
-    setEnteredGoalText(enteredText);
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText: string) {
     setGoals([...goals, { text: enteredGoalText, key: Math.random() }]);
   }
 
   return (
     <View className="flex-1 pt-[10%] items-center bg-neutral-600">
-      <View className="flex flex-row justify-between mt-4 pb-8 mb-4 border-b-gray-500 border-b-[1.5px]">
-        <GoalInput goalInputHandler={goalInputTextHandler} />
-        <GoalAddButton addGoalHandler={addGoalHandler} />
-      </View>
+      <GoalInput addGoal={addGoalHandler} />
+
       <View className="self-stretch">
         <FlatList
           className="flex pt-4 mb-4"

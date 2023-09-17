4609f44051755735dbd914dfd89c26b984e84e6e
Kiki <caiquelsobral@gmail.com>
2023-09-17 13:15:19 -0300

    Refac: Replace ScrollView with FlatList

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import {
+  FlatList,
   ScrollView,
   Text,
   TextInput,
@@ -7,24 +8,31 @@ import {
   View,
 } from 'react-native';
 
+let nextId = 0;
+
 export default function App() {
+  type goalType = {
+    text: string;
+    key: number;
+  };
+
   const [enteredGoalText, setEnteredGoalText] = useState<string>('');
-  const [goals, setGoals] = useState<string[]>([]);
+  const [goals, setGoals] = useState<Array<goalType>>([]);
 
   function goalInputTextHandler(enteredText: string) {
     setEnteredGoalText(enteredText);
   }
 
   function addGoalHandler() {
-    setGoals((currentGoals: string[]) => [...currentGoals, enteredGoalText]);
+    setGoals([...goals, { text: enteredGoalText, key: Math.random() }]);
   }
 
   return (
     <View className="flex-1 pt-[10%] items-center bg-neutral-600">
       <View className="flex flex-row justify-between mt-4 pb-8 mb-4 border-b-gray-500 border-b-[1.5px]">
         <TextInput
-          className="border-[1.5px] w-[60%] mr-[5%] border-gray-400 rounded-xl pl-4 text-lg text-gray-50"
-          placeholder="Your course goal!"
+          className="border-[1.5px] w-[60%] mr-[5%] border-gray-400 rounded-lg pl-4 text-lg text-gray-50"
+          placeholder="New goal"
           placeholderTextColor={'#999'}
           onChangeText={goalInputTextHandler}
         ></TextInput>
@@ -36,16 +44,19 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <View className="self-stretch">
-        <ScrollView className="flex pt-4 gap-4">
-          {goals.map((goal, i) => (
-            <View
-              className="self-center flex items-center justify-center h-16 bg-neutral-400 w-[80%] rounded-xl"
-              key={i}
-            >
-              <Text className="text-2xl text-gray-50">{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          className="flex pt-4 mb-4"
+          data={goals}
+          renderItem={(goalData) => {
+            return (
+              <View className="self-center justify-self-center flex items-center justify-center h-16 mb-6 bg-neutral-400 w-[80%] rounded-xl">
+                <Text className="text-2xl text-gray-50">
+                  {goalData.item.text}
+                </Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );

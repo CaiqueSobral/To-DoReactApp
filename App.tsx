d19b32bfec7f7debf4f5ae520aa237701a908b6b
Kiki <caiquelsobral@gmail.com>
2023-09-16 22:33:53 -0300

    Feat: Add functionality to add goals

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState<string>('');
+  const [goals, setGoals] = useState<string[]>([]);
+
+  function goalInputTextHandler(enteredText: string) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setGoals((currentGoals: string[]) => [...currentGoals, enteredGoalText]);
+  }
+
   return (
-    <View className="flex-1 pt-[10%] items-center bg-gray-600">
+    <View className="flex-1 pt-[10%] items-center bg-neutral-600">
       <View className="flex flex-row justify-between mt-4 pb-8 mb-4 border-b-gray-500 border-b-[1.5px]">
         <TextInput
-          className="border-[1.5px] w-[60%] mr-[5%] border-gray-400 rounded-xl pl-4 text-lg"
+          className="border-[1.5px] w-[60%] mr-[5%] border-gray-400 rounded-xl pl-4 text-lg text-gray-50"
           placeholder="Your course goal!"
           placeholderTextColor={'#999'}
+          onChangeText={goalInputTextHandler}
         ></TextInput>
-        <TouchableOpacity className="py-1.5 px-6 bg-gray-400 rounded-2xl">
+        <TouchableOpacity
+          className="py-1.5 px-6 bg-neutral-400 rounded-2xl"
+          onPress={addGoalHandler}
+        >
           <Text className="text-gray-200 text-l">Add Goal</Text>
         </TouchableOpacity>
       </View>
-      <View className="flex items-center grow w-full">
-        <Text className="text-gray-200">List of Goals</Text>
+      <View className="self-stretch">
+        <ScrollView className="flex pt-4 gap-4">
+          {goals.map((goal, i) => (
+            <View
+              className="self-center flex items-center justify-center h-16 bg-neutral-400 w-[80%] rounded-xl"
+              key={i}
+            >
+              <Text className="text-2xl text-gray-50">{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
